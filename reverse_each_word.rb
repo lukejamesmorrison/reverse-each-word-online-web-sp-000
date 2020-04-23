0b8012676b5c52c5e5d8e557070b04b8ecb2d724
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-23 19:46:42 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word(sentence)
+  
+  new_array = sentence.split(' ').collect do |word|
+    reversed_array = []
+    word.split('').each do |letter|
+      reversed_array.unshift(letter)
+    end
+    reversed_array.join
+  end
+  
+  new_array.join(' ')
+end
