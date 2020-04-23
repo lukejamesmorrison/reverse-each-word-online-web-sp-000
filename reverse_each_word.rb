021c58b481d2cc5121dccf2f43275e63e34edcb1
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-23 19:43:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,17 @@
 def reverse_each_word(sentence)
   words_array = sentence.split(' ')
+  new_array= []
   
   words_array.collect do |word|
     word_array = word.split('')
+    reversed_array = []
     
-    word_array.collect do |letter|
+    word_array.each do |letter|
       reversed_array.unshift(letter)
     end
     
-    new_array << word_array.join
+    new_array << reversed_array.join
   end
   
-  words_array.join(' ')
+  new_array.join(' ')
 end
